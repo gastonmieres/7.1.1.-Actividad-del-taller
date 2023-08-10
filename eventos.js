8642061764c1e9974217f709da0f03797cd8e32f
gastonmieres <g4ston.m1eres@gmail.com>
2023-08-10 20:44:38 -0300

    Cambios en el index.html

--- eventos.js
+++ eventos.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const miDiv = document.getElementById("miDiv");
 
     miDiv.addEventListener("click", () => {
-        event.stopPropagation();
         alert("¡Hola! Soy el DIV");
     });
 });
