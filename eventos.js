9950193adf673d314ea304976b76dcf58b659af0
gastonmieres <g4ston.m1eres@gmail.com>
2023-08-10 19:26:11 -0300

    Implementación de "event.stopPropagation();"

--- eventos.js
+++ eventos.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const miDiv = document.getElementById("miDiv");
 
     miDiv.addEventListener("click", () => {
+        event.stopPropagation();
         alert("¡Hola! Soy el DIV");
     });
 });
