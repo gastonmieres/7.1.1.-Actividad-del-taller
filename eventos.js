522354c6ef21636fc1d93d0d59aa7e1c98b2a05c
gastonmieres <g4ston.m1eres@gmail.com>
2023-08-10 16:37:46 -0300

    Implementacion del "eventos.js"

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const miDiv = document.getElementById("miDiv");
+
+    miDiv.addEventListener("click", () => {
+        alert("Hola! Soy el div");
+    });
+});
